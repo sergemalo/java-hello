dedf52e7f834d3a5c7cae0d4788735a88424b06b
Serge Malo <sergemalo2000@outlook.com>
2024-09-13 09:04:53 -0400

    Added a class

--- demo/src/main/java/ca/umontreal/ift2015/hello/Main.java
+++ demo/src/main/java/ca/umontreal/ift2015/hello/Main.java
@@ -1,7 +1,12 @@
 package ca.umontreal.ift2015.hello;
 
+import ca.umontreal.ift2015.hello.my_class_a;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        my_class_a a = new my_class_a();
+        a.toto();
     }
 }
---
+++ demo/src/main/java/ca/umontreal/ift2015/hello/my_class_a.java
@@ -0,0 +1,9 @@
+package ca.umontreal.ift2015.hello;
+
+public class my_class_a {
+    public int     toto(){
+        System.out.println("toto!");
+        return 0;
+    }
+
+}
