266f296b8948830c532e2f2d969a592a01c3563a
Serge Malo <sergemalo2000@outlook.com>
2024-09-13 09:06:46 -0400

    Added file headers

--- demo/src/main/java/ca/umontreal/ift2015/hello/my_class_a.java
+++ demo/src/main/java/ca/umontreal/ift2015/hello/my_class_a.java
@@ -1,5 +1,38 @@
 package ca.umontreal.ift2015.hello;
 
+
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2024 Serge Malo
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+* JAVA Hello World - my_class_a
+*
+* @author Serge Malo
+* @version 1.0
+* @since 1.0, 2024.09.13
+*/
+
 public class my_class_a {
     public int     toto(){
         System.out.println("toto!");
